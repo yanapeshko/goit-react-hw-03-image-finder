3faf5804adb433a7c006d7f7eb1cdfc3d4e3ed98
yanapeshko <yanapeshko917@gmail.com>
2021-12-20 11:16:54 +0300

    -comments

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,13 +1,3 @@
-// import URL from './settings';
-
-// export default async function fetchArticles(imageRequest, page) {
-//   const url = `${URL.BASE_URL}/?image_type=${URL.IMAGE_TYPE}&orientation=${URL.ORIENTATION}&q=${imageRequest}&page=${page}&per_page=${URL.QUANTITY_PER_PAGE}&key=${URL.KEY}`;
-
-//   const response = await fetch(url);
-//   const res = await response.json();
-//   return await res.hits;
-// }
-
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
